6b9c699c4bd1f68af8dc018d2c1a23af19b5253f
Magno Daniel <magzoa@hotmail.com>
2016-02-03 20:02:14 -0300

    modificación externa

--- src/main/java/br/com/yaw/demomaven/App.java
+++ src/main/java/br/com/yaw/demomaven/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World in Paraguay por segunda vez!" );
+        System.out.println( "Hello World in Paraguay por segunda vez para un usuario pendejo!" );
     }
 }
